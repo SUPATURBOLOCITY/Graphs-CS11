bcbfc60c936612b836f9872ba88e78156c15dd90
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-09-12 12:51:10 -0400

    Adding vertex class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,45 +2,60 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+class Vertex:
+    def __init__(self, vertex_id, x=None, y=None, value=None, color="white"):
+        self.id = int(vertex_id)
+        self.x = x
+        self.y = y
+        self.value = value
+        self.color = color
+        self.edges = set()
+        if self.x is None:
+            self.x = self.id
+        if self.y is None:
+            self.y = self.id
+        if self.value is None:
+            self.value = self.id
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
     def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = set()
+        self.vertices[vertex_id] = Vertex(vertex_id)
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
-            self.vertices[v2].add(v1)
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
         else:
             raise IndexError("That vertex does not exist!")
     def add_directed_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
+            self.vertices[v1].edges.add(v2)
         else:
             raise IndexError("That vertex does not exist!")
 
 
 
 
-def dft(adjList, node_id, visited):
-    visited.append(node_id)
-    for child_node in adjList[node_id]:
-        if child_node not in visited:
-            dft(adjList, child_node, visited)
+# def dft(adjList, node_id, visited):
+#     visited.append(node_id)
+#     for child_node in adjList[node_id]:
+#         if child_node not in visited:
+#             dft(adjList, child_node, visited)
 
 
-def bft(adjList, node_id):
-    frontier = []
-    frontier.append(node_id)
-    visited = []
-    while len(frontier) > 0:
-        n = frontier.pop(0)
-        if n not in visited:
-          print(n)
-          visited.append(n)
-          for next_node in adjList[n]:
-              frontier.append(next_node)
+# def bft(adjList, node_id):
+#     frontier = []
+#     frontier.append(node_id)
+#     visited = []
+#     while len(frontier) > 0:
+#         n = frontier.pop(0)
+#         if n not in visited:
+#           print(n)
+#           visited.append(n)
+#           for next_node in adjList[n]:
+#               frontier.append(next_node)
 
 
