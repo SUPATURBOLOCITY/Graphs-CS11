aba15db04e37ef4e9da4f775a94f1c6fd71c3fab
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-09-12 13:35:29 -0400

    Parsing args

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -8,7 +8,8 @@ from sys import argv
 from graph import Graph
 from draw import BokehGraph
 
-def main():
+def main(**kwargs):
+    print(kwargs)
     graph = Graph()  # Instantiate your graph
     graph.add_vertex(0)
     graph.add_vertex(1)
@@ -36,5 +37,19 @@ def main():
 
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+
+    style="default"
+    num_verts = 5
+    num_edges = 6
+
+    for arg in argv[1:]:
+        arg_split = arg.split("=")
+        if len(arg_split) == 2:
+            if arg_split[0] == "style":
+                style = arg_split[1].lower()
+            elif arg_split[0] == "verts":
+                num_verts = int(arg_split[1])
+            elif arg_split[0] == "edges":
+                num_edges = int(arg_split[1])
+
+    main(style=style, num_verts=num_verts, num_edges=num_edges)
